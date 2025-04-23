b3e5926f0007e783aa515d452c42e225a7e16385
Suhas Subramani <ssubramani@sunovaa.in>
2025-04-23 16:15:44 +0530

    added all files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Suhas",
+    author_email="suhaspanja@gmail.com",
+    packages=find_packages()
+)
